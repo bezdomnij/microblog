ff2b1013e3a474ed57a7bdeb2d2d4b610bbfa105
bezdomnij <attlila.novak@yahoo.co.uk>
2021-04-14 10:03:35 +0200

    ch4: set up for db connection

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,12 @@
 from flask import Flask
 from config import Config  # adding config class / import from config module
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Config)  # import config from Config class
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-from app import routes  # here, to prevent circular imports // why not at the top
+
+from app import routes, models  # here, to prevent circular imports // why not at the top
