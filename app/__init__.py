64b929b47028061a950427725b98ae3a085742fb
bezdomnij <attlila.novak@yahoo.co.uk>
2021-04-15 16:13:39 +0200

    updated routes/login path

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,14 @@ from flask import Flask
 from config import Config  # adding config class / import from config module
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
+
 
 app = Flask(__name__)
 app.config.from_object(Config)  # import config from Config class
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
-
+login = LoginManager(app)
+login.login_view = 'login'
 
 from app import routes, models  # here, to prevent circular imports // why not at the top
